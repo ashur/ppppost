663da537c7a74af2e089339a7aaccaa25ad34edd
Ashur Cabrera <ashur@panic.com>
2018-11-30 16:36:03 -0800

    (WIP) Implement add command

---
+++ bin/ppppost-add.js
@@ -0,0 +1,109 @@
+#!/usr/bin/env node
+
+const {bootstrap, Config} = require( '../src/helpers/app-from-config-path' );
+const Bot = require( '../src/bot' );
+const Mastodon = require( '../src/mastodon' );
+const pkg = require( '../package.json' ); 
+const program = require( 'commander' );
+const readline = require('readline');
+
+program
+	.command( 'add <bot>', 'add a new bot' )
+	.option( '-M, --mastodon', 'post only to Mastodon' )
+	.option( '-T, --twitter', 'post only to Twitter' )
+	.parse( process.argv );
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+function prompt( message )
+{
+	return new Promise( resolve =>
+	{
+		rl.question( `${message}: `, answer =>
+		{
+			if( answer == '' )
+			{
+				resolve( prompt( message ) );
+			}
+
+			resolve( answer );
+		});
+	});
+}
+
+/**
+ * Run the command
+ */
+let { PPPPOST_CONFIG:configPath = Config.DEFAULT_PATH } = process.env;
+
+bootstrap( configPath )
+	.then( app =>
+	{
+		let botName = program.args[0];
+		let botArgs = {
+			name: botName
+		};
+
+		// TODO: See if bot already exists with the same name
+
+		(function()
+		{
+			return new Promise( resolve =>
+			{
+				if( program.mastodon )
+				{
+					let data = {};
+
+					prompt( 'Mastodon Instance (ex., mastodon.social)' )
+						.then( instance =>
+						{
+							// TODO: Remove trailing '/' from instance
+
+							data.api_url = `https://${instance}/api/v1/`;
+
+							return prompt( 'Mastodon Token' );
+						})
+						.then( access_token =>
+						{
+							data.access_token = access_token;
+
+							botArgs.mastodon = new Mastodon( data );
+
+							return prompt( 'Mastodon Visibility' );
+						})
+						.then( visibility =>
+						{
+							data.visibility = visibility;
+
+							botArgs.mastodon = new Mastodon( data );
+							resolve();
+						})
+				}
+				else
+				{
+					resolve();
+				}
+			});
+		})()
+		.then( () =>
+		{
+			// TODO: Add Twitter support
+		})
+		.then( () =>
+		{
+			rl.close();
+
+			let bot = new Bot( botArgs );
+			console.log( bot );
+
+			// TODO: Implement saving new/updated bot to disk
+		});
+	})
+	.catch( error =>
+	{
+		console.log( `${pkg.name}: ${error}` );
+		process.exit( 1 );
+	})
--- bin/ppppost.js
+++ bin/ppppost.js
@@ -5,6 +5,7 @@ const pkg = require( '../package.json' );
 
 program
 	.version( `${pkg.name} version ${pkg.version}` )
+	.command( 'add <bot>', 'add a new bot' )
 	.command( 'list', 'list all bots' ).alias( 'ls' )
 	.command( 'to <bot>', 'post a message' )
 	.parse( process.argv );
